6d5245c7f97a5f311177c0dd368968e2a164b931
Pooya Parsa <pooya@pi0.io>
2025-10-10 05:47:48 +0200

    chore: update starter

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,8 +1,4 @@
 import { defineNitroConfig } from "nitro/config"
 
-// https://nitro.build/config
 export default defineNitroConfig({
-  compatibilityDate: "latest",
-  srcDir: "server",
-  imports: false
 });
--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,10 @@
-import { defineHandler } from "h3"
+import { defineHandler, html } from "nitro/deps/h3"
 
-// Learn more: https://nitro.build/guide/routing
 export default defineHandler((event) => {
-  return `
+  return html`
       <meta charset="utf-8">
       <h1>This is your brand new Nitro project 🚀 </h1>
       <p>Get started by editing the <code>server/routes/index.ts</code> file.</p>
-      <p>Learn more from 📖 <a href="https://nitro.build/guide" target="_blank">Nitro Documentation</a></p>
+      <p>Learn more from 📖 <a href="https://v3.nitro.build/guide" target="_blank">Nitro Documentation</a></p>
     `;
 });
